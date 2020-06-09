51b3e0a6a70d4f0f4eb6c068016b0eb3e2712716
Beneba <afetsib@gmail.com>
2020-06-09 04:37:42 -0700

    first phase

---
+++ js/custom.js
@@ -0,0 +1,120 @@
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+	
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
+    
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+	
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	$('#slides').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 10000,
+		animation: 'fade',
+	});
+	
+	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
+	
+	/* ..............................................
+    Map Full
+    ................................................. */
+	
+	$(document).ready(function(){ 
+		$(window).on('scroll', function () {
+			if ($(this).scrollTop() > 100) { 
+				$('#back-to-top').fadeIn(); 
+			} else { 
+				$('#back-to-top').fadeOut(); 
+			} 
+		}); 
+		$('#back-to-top').click(function(){ 
+			$("html, body").animate({ scrollTop: 0 }, 600); 
+			return false; 
+		}); 
+	});
+	
+	/* ..............................................
+    Special Menu
+    ................................................. */
+	
+	var Container = $('.container');
+	Container.imagesLoaded(function () {
+		var portfolio = $('.special-menu');
+		portfolio.on('click', 'button', function () {
+			$(this).addClass('active').siblings().removeClass('active');
+			var filterValue = $(this).attr('data-filter');
+			$grid.isotope({
+				filter: filterValue
+			});
+		});
+		var $grid = $('.special-list').isotope({
+			itemSelector: '.special-grid'
+		});
+	});
+	
+	/* ..............................................
+    BaguetteBox
+    ................................................. */
+	
+	baguetteBox.run('.tz-gallery', {
+		animation: 'fadeIn',
+		noScrollbars: true
+	});
+	
+	
+	
+	/* ..............................................
+    Datepicker
+    ................................................. */
+	
+	$('.datepicker').pickadate();
+	
+	$('.time').pickatime();
+	
+	
+	
+	
+	$(document).ready(setInterval(function ()
+	{
+		$("#communicate").delay(0).animate({
+			left: '23px',
+			top: '8px'
+		});
+	
+		$("#train").delay(500).animate({
+			left: '121px',
+			top: '50px'
+		});
+	
+		$("#educate").delay(1000).animate({
+			left: '56px',
+			top: '97px'
+		});
+	
+		$("#communicate, #train, #educate ").css("left", "-180px");
+	
+	}, 2000));
+	
+}(jQuery));
+
